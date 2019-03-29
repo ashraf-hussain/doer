dbe90c2eb06cef17d1cdf07c9efc52671f9871ef
Ashraf Hussain <ashraf@Ashrafs-MacBook-Pro.local>
2019-03-29 01:54:27 -0400

    Designed adding new group with adppter layout and added tab layout for user.

---
+++ app/src/main/java/com/project/doer/AdminDashboardActivity.java
@@ -0,0 +1,166 @@
+package com.project.doer;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.project.doer.userSignUp.SignUpActivity;
+
+public class AdminDashboardActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = AdminDashboardActivity.class.getName();
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        this.getActionBar().hide();
+
+        notificationBarSetup();
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_create_group);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Let's create an awesome group !", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                Intent intent = new Intent(AdminDashboardActivity.this, CreateGroupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "In the onStart() event");
+    }
+
+    public void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "In the onRestart() event");
+    }
+
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "In the onResume() event");
+    }
+
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "In the onPause() event");
+    }
+
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "In the onStop() event");
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "In the onDestroy() event");
+    }
+
+
+    //NotificationBar setup
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void notificationBarSetup() {
+        Window window = this.getWindow();
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+    }
+
+
+}
---
+++ app/src/main/java/com/project/doer/userReview/UserReviewFragment.java
@@ -0,0 +1,95 @@
+package com.project.doer.userReview;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.project.doer.R;
+import com.project.doer.common.BaseFragment;
+import com.project.doer.common.ConnectionDetector;
+import com.project.doer.data.AppUtils;
+import com.project.doer.userTask.UserTaskFragment;
+
+public class UserReviewFragment extends BaseFragment {
+
+    // TaskAdapter taskAdapter;
+//    @BindView(R.id.rv_user_task_fragment)
+//    RecyclerView rvRockFragment;
+    ConnectionDetector connectionDetector;
+    //    @BindView(R.id.ll_offline_mode)
+//    LinearLayout tvOfflineMode;
+//    // UserTaskPresenter userTaskPresenter;
+//    @BindView(R.id.swipe_refresh)
+   //SwipeRefreshLayout pullToRefresh;
+    private static final String TAG = UserReviewFragment.class.getSimpleName();
+
+//
+//    @Override
+//    public void showRecyclerView(List<MusicModel> musicModelList) {
+//
+//        musicAdapter = new MusicAdapter(musicModelList);
+//        rvRockFragment.setAdapter(musicAdapter);
+//    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_user_task_review;
+    }
+
+    @Override
+    protected void init() {
+        connectionDetector = new ConnectionDetector(getContext());
+
+        //rvRockFragment.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        //rvRockFragment.setLayoutManager(layoutManager);
+
+        // userTaskPresenter = new UserTaskImp(this);
+        // userTaskPresenter.loadUserTask();
+
+    }
+
+    //swipe to refresh
+//    private void swipeToRefresh() {
+//        if (!connectionDetector.isConnected()) {
+//            // tvOfflineMode.setVisibility(View.VISIBLE);
+//
+//            pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+//                @Override
+//                public void onRefresh() {
+//                    AppUtils.showSnackbar(pullToRefresh, "No Internet Connection !");
+//                    pullToRefresh.setRefreshing(false);
+//                }
+//            });
+//
+//        } else {
+//            pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+//                @Override
+//                public void onRefresh() {
+//                    //  userTaskPresenter.loadUserTask();
+//                    //   tvOfflineMode.setVisibility(View.GONE);
+//                    pullToRefresh.setRefreshing(false);
+//                    AppUtils.showSnackbar(pullToRefresh, "Data Refreshed");
+//                }
+//            });
+//
+//        }
+//
+//    }
+//
+//    private void checkpoint() {
+//
+//        if (!connectionDetector.isConnected()) {
+//            AppUtils.showSnackbar(pullToRefresh, "No Internet Connection !");
+//        }
+//    }
+//
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        checkpoint();
+//        swipeToRefresh();
+//    }
+//
+}
